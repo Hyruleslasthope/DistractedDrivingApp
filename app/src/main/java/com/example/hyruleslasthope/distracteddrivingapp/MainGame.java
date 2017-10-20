4d1cd2f457d603799fa38b6e92fb761bd6d75c32
Thomas <thomasb.h@hotmail.com>
2017-10-20 14:13:40 -0400

    ImageView Moves
    Added fuctionality to the buttons and image view, Buttons add and remove a Margin.

--- app/src/main/java/com/example/hyruleslasthope/distracteddrivingapp/MainGame.java
+++ app/src/main/java/com/example/hyruleslasthope/distracteddrivingapp/MainGame.java
@@ -1,7 +1,11 @@
 package com.example.hyruleslasthope.distracteddrivingapp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainGame extends AppCompatActivity {
@@ -12,7 +16,28 @@ public class MainGame extends AppCompatActivity {
         setContentView(R.layout.activity_main_game);
 
 
-        ImageView myImageView = (ImageView) findViewById(R.id.imageView);
+        final ImageView myImageView = (ImageView) findViewById(R.id.imageView);
+        Button upButton = (Button) findViewById(R.id.upBtn);
+        Button downButton= (Button) findViewById(R.id.dwnBtn);
+
+        ImageView pylon = new ImageView(MainGame.this);
+        pylon.setImageResource(R.drawable.Pylon);
+
+        downButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ViewGroup.MarginLayoutParams)myImageView.getLayoutParams()).topMargin = 300;
+                myImageView.requestLayout();
+            }
+        });
+
+        upButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ViewGroup.MarginLayoutParams)myImageView.getLayoutParams()).topMargin = 0;
+                myImageView.requestLayout();
+            }
+        });
 
 
 
