bba5ab5f705b07fc6ef46bf1af421cb048601f6d
Thomas <thomasb.h@hotmail.com>
2017-10-25 19:16:47 -0400

    Animation!
    Changed the car animation to animate properly instead of adding a margin. Created an obstical animation that randomly picks a "lane" to go down and crosses the screen.

--- app/src/main/java/com/example/hyruleslasthope/distracteddrivingapp/MainGame.java
+++ app/src/main/java/com/example/hyruleslasthope/distracteddrivingapp/MainGame.java
@@ -1,12 +1,19 @@
 package com.example.hyruleslasthope.distracteddrivingapp;
 
-import android.media.Image;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import javax.xml.datatype.Duration;
 
 public class MainGame extends AppCompatActivity {
 
@@ -20,26 +27,105 @@ public class MainGame extends AppCompatActivity {
         Button upButton = (Button) findViewById(R.id.upBtn);
         Button downButton= (Button) findViewById(R.id.dwnBtn);
 
-        ImageView pylon = new ImageView(MainGame.this);
-        pylon.setImageResource(R.drawable.Pylon);
+        final ImageView pylon = (ImageView) findViewById(R.id.obstical);
+
+        pylon.setVisibility(View.GONE);
+
+        final Animation obUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.obsticalmove_uppos);
+
+        final Animation obDwn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.obsticalmove_dwnpos);
+
+        pylon.setX(1500);
+        obUp.setFillAfter(true);
+        obDwn.setFillAfter(true);
+        pylon.startAnimation(obDwn);
+
+        final Animation moveDown = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.carmove_down);
+
+
+        obUp.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                pylon.setX(1500);
+                pylon.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                if(RanNum() % 2 == 0) {
+                    pylon.startAnimation(obDwn);
+                }else{
+                    pylon.startAnimation(obUp);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        obDwn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                pylon.setX(1500);
+                pylon.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                if(RanNum() % 2 == 1) {
+                    pylon.startAnimation(obUp);
+                }else{
+                    pylon.startAnimation(obDwn);
+                }
+                }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        final Animation moveUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.carmove_up);
 
         downButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((ViewGroup.MarginLayoutParams)myImageView.getLayoutParams()).topMargin = 300;
-                myImageView.requestLayout();
+              //  ((ViewGroup.MarginLayoutParams)myImageView.getLayoutParams()).topMargin = 300;
+                //myImageView.requestLayout();
+                myImageView.startAnimation(moveDown);
             }
         });
 
         upButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((ViewGroup.MarginLayoutParams)myImageView.getLayoutParams()).topMargin = 0;
-                myImageView.requestLayout();
+             //   ((ViewGroup.MarginLayoutParams)myImageView.getLayoutParams()).topMargin = 0;
+               // myImageView.requestLayout();
+                myImageView.startAnimation(moveUp);
             }
         });
 
+        Toast toast = Toast.makeText(getApplicationContext(), "Hit", Toast.LENGTH_SHORT);
 
+        Rect r1 = new Rect();
+        Rect r2 = new Rect();
 
+        myImageView.getHitRect(r1);
+        pylon.getHitRect(r2);
+
+        if(Rect.intersects(r2,r1)){
+            toast.show();
+        }
     }
+
+    private int RanNum(){
+        Random ran = new Random();
+        int r = ran.nextInt(11-1)+ 1;
+        return r;
+    }
+
 }
